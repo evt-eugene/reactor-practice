779fc28471cadee072a259a776eee10c873daf75
Eugene Yevtushenko <evt.eugene@gmail.com>
2022-06-19 00:18:58 +0300

    fix: incorrect configuration

--- spring-web-app/src/main/java/com/example/demo/CassandraConfig.java
+++ spring-web-app/src/main/java/com/example/demo/CassandraConfig.java
@@ -1,42 +1,47 @@
 package com.example.demo;
 
-import com.example.demo.student.persistance.converters.CassandraIntToYearConverter;
-import com.example.demo.student.persistance.converters.CassandraYearToIntConverter;
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.data.cassandra.config.AbstractReactiveCassandraConfiguration;
-import org.springframework.data.cassandra.core.convert.CassandraCustomConversions;
-import org.springframework.data.cassandra.core.cql.session.init.KeyspacePopulator;
-import org.springframework.data.cassandra.core.cql.session.init.ResourceKeyspacePopulator;
+import org.springframework.util.FileCopyUtils;
 
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UncheckedIOException;
+import java.nio.charset.Charset;
 import java.util.List;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
 
 @Configuration
 public class CassandraConfig extends AbstractReactiveCassandraConfiguration {
 
-  @Override
-  protected KeyspacePopulator keyspacePopulator() {
-    var filePopulator = new ResourceKeyspacePopulator();
-    filePopulator.setSeparator(";");
-    filePopulator.setScripts(new ClassPathResource("cql/db-schema.cql"));
+  @Autowired
+  private ResourceLoader resourceLoader;
 
-    return filePopulator;
+  @Override
+  protected List<String> getStartupScripts() {
+    return List.of(
+        loadResourceAsString("classpath:cql/create_keyspace.cql", UTF_8),
+        loadResourceAsString("classpath:cql/create_books_table.cql", UTF_8),
+        loadResourceAsString("classpath:cql/create_books_title_index.cql", UTF_8)
+    );
+  }
+
+  private String loadResourceAsString(String location, Charset charset) {
+    var resource = resourceLoader.getResource(location);
+
+    try (var reader = new InputStreamReader(resource.getInputStream(), charset)) {
+      return FileCopyUtils.copyToString(reader);
+    } catch (IOException e) {
+      throw new UncheckedIOException(e);
+    }
   }
 
   @Override
   protected String getKeyspaceName() {
     return "library";
-  }
-
-  @Bean
-  public CassandraCustomConversions customConversions() {
-    return new CassandraCustomConversions(
-        List.of(
-            CassandraIntToYearConverter.INSTANCE,
-            CassandraYearToIntConverter.INSTANCE
-        )
-    );
   }
 
 }
